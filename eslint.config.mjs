ee18245a50ee92ea8a93043e34c2dedaa91755fb
lonewolfyx <877085893@qq.com>
2026-04-13 14:15:40 +0800

    refactor(eslint): update eslint configuration to use antfu config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,24 @@
-// @ts-check
+import antfu from '@antfu/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-    // Your custom configs here
-)
+export default antfu({
+    vue: {
+        overrides: {
+            'vue/block-order': [
+                'error',
+                {
+                    order: ['template', 'script', 'style'],
+                },
+            ],
+        },
+    },
+    stylistic: {
+        indent: 4,
+        quotes: 'single',
+    },
+    yaml: {
+        overrides: {
+            'yaml/indent': ['error', 2],
+        },
+    },
+}).append(withNuxt)
