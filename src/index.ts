a0c537e2c3636afec844eeff595d15791f2aa007
lonewolfyx <877085893@qq.com>
2026-04-15 22:29:19 +0800

    refactor(cli): update CLI action to use resolved configuration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import process from 'node:process'
+import type { IOptions } from '~~/src/types'
 import cac from 'cac'
+import { resolveConfig } from '~~/src/config'
 import { name, version } from '../package.json' with { type: 'json' }
 
 const cli = cac(name)
@@ -8,8 +9,9 @@ cli.command('', 'Start tokens usage analysis')
     .option('--host <host>', 'Host', { default: '127.0.0.1' })
     .option('--port <port>', 'Port', { default: 7777 })
     .option('--open', 'Open browser', { default: true })
-    .action(() => {
-        console.log(process.cwd())
+    .action((options: IOptions) => {
+        const config = resolveConfig(options)
+        console.log(config)
     })
 
 cli.help()
