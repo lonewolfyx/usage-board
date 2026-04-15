fd0e048cb660c2d14cfe141e20837d1db6cb47d4
lonewolfyx <877085893@qq.com>
2026-04-15 14:11:58 +0800

    feat(app): set default page title for tokens usage analysis

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,7 @@ export default defineNuxtConfig({
 
     app: {
         head: {
+            title: 'Tokens Usage Analysis',
             viewport: 'width=device-width,initial-scale=1',
             link: [
                 { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
