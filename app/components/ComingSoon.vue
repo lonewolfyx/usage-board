0d09759f74dbd0084a4183605e584d5892fcf9d4
lonewolfyx <877085893@qq.com>
2026-04-15 14:25:27 +0800

    feat(pages): add coming soon pages for various features

---
+++ app/components/ComingSoon.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="grow container mx-auto">
+        <div class="flex justify-center items-center flex-col text-center h-full gap-6 ">
+            <h2 class="text-4xl md:text-5xl lg:text-6xl font-bold text-balance max-w-4xl">
+                The future is almost here
+            </h2>
+            <p class="text-lg md:text-xl text-balance max-w-2xl text-muted-foreground">
+                We're putting the finishing touches on something extraordinary. Get ready for a new experience.
+            </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+defineOptions({
+    name: 'ComingSoon',
+})
+</script>
