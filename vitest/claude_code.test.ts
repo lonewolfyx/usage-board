3b81db4251c66a425041a37110450249947d1e04
lonewolfyx <877085893@qq.com>
2026-04-16 16:22:40 +0800

    test(claude): add test case for Claude code usage functionality

---
+++ vitest/claude_code.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest'
+import { resolveConfig } from '../src/config'
+import { loadClaudeCodeUsage } from '../src/platform'
+
+const config = resolveConfig({
+    host: '127.0.0.1',
+    port: 8888,
+    open: false,
+})
+
+describe('test claude code', () => {
+    it('should ', async () => {
+        const data = await loadClaudeCodeUsage(config)
+        await expect(data).toMatchFileSnapshot('./claude.json')
+    })
+})
