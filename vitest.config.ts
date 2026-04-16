bc757961668eec375d8504dd752817f3b22d51a3
lonewolfyx <877085893@qq.com>
2026-04-16 10:33:09 +0800

    chore(test): enable update mode in vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
         },
     },
     test: {
+        update: true,
         coverage: {
             provider: 'v8',
             reporter: ['text', 'json', 'html'],
